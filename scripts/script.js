0cc4148062a7fc0ddf3aa662dda5ffa36688a38b
trekitch <trekitch@ymail.com>
2022-01-28 23:59:45 -0500

    added functions for basic operations

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,43 @@
-console.log("Hello World")
+function add(x, y) {
+    let sum = x + y;
+    console.log(sum)
+}
+
+function subtract(x, y) {
+    let diff = x - y;
+    console.log(diff)
+}
+
+function multiply(x, y) {
+    let product = x * y;
+    console.log(product);
+}
+
+function divide(x, y) {
+    let quotient = x / y;
+    console.log(quotient);
+}
+
+function operate(operator, x, y) {
+    switch (operator) {
+        case "+":
+            add(x, y)
+            break;
+        case "-":
+            subtract(x, y)
+            break;
+        case "*":
+            multiply(x, y)
+            break;
+        case "/":
+            divide(x, y)
+            break;
+        default:
+            break;
+    }
+}
+
+add(4, 2);
+subtract(10, 5);
+multiply(2, 6);
+divide(10, 2);
