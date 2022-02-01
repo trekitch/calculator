ef814bb5bc015196e081bbe91061171e73646d4f
trekitch <trekitch@ymail.com>
2022-02-01 03:55:29 -0500

    added logic for display and calculations

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,25 @@
+let operators = "";
+
+function addBtnListeners() {
+    let buttons = document.querySelector(".button_container").children;
+    let buttonsArray = Array.from(buttons);
+
+    buttonsArray.forEach(function (button) {
+        button.addEventListener("click", function () {
+            updateDisplay(button.textContent);
+            storeOperator(button.textContent);
+        });
+    });
+}
+
 function add(x, y) {
     let sum = x + y;
-    console.log(sum)
+    console.log(sum);
 }
 
 function subtract(x, y) {
     let diff = x - y;
-    console.log(diff)
+    console.log(diff);
 }
 
 function multiply(x, y) {
@@ -21,23 +35,54 @@ function divide(x, y) {
 function operate(operator, x, y) {
     switch (operator) {
         case "+":
-            add(x, y)
+            add(x, y);
             break;
         case "-":
-            subtract(x, y)
+            subtract(x, y);
             break;
         case "*":
-            multiply(x, y)
+            multiply(x, y);
             break;
         case "/":
-            divide(x, y)
+            divide(x, y);
             break;
         default:
             break;
     }
 }
 
-add(4, 2);
-subtract(10, 5);
-multiply(2, 6);
-divide(10, 2);
+function updateDisplay(number) {
+    const displayText = document.querySelector(".display");
+    if (number === "C") {
+        displayText.textContent = "0";
+    } else {
+        displayText.textContent = number;
+    }
+}
+
+function storeOperator(nums) {
+    operators += nums;
+    console.log(nums);
+    console.log(operators);
+}
+
+function evaluateExpression(exp) {
+    let expCopy;
+    if (exp.includes("+")) {
+        expCopy = exp.split("+");
+        operate("+", +expCopy[0], +expCopy[1]);
+    } else if (exp.includes("-")) {
+        expCopy = exp.split("-");
+        operate("-", +expCopy[0], +expCopy[1]);
+    } else if (exp.includes("*")) {
+        expCopy = exp.split("*");
+        operate("*", +expCopy[0], +expCopy[1]);
+    } else {
+        expCopy = exp.split("/");
+        operate("/", +expCopy[0], +expCopy[1]);
+    }
+
+    console.log(expCopy);
+}
+
+window.onload = addBtnListeners();
